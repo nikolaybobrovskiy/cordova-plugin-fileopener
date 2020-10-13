9352d369fbb601ac93f62da6292ec46160e5e773
Nikolay Bobrovskiy <nikolay.bobrovskiy@gmail.com>
2020-10-13 21:31:05 +0300

    DBN-12112

--- src/android/FileOpener.java
+++ src/android/FileOpener.java
@@ -160,13 +160,13 @@ public class FileOpener extends CordovaPlugin {
     private Intent generateOpenFileIntent(File tempFile, String extension, Context context) {
         Uri contentUri = null;
         Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             contentUri = FileProvider.getUriForFile(context, cordova.getActivity().getPackageName() + ".fileProvider", tempFile);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             intent.setDataAndType(contentUri, getMimeType(extension));
         } else {
             contentUri = Uri.fromFile(tempFile);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.setDataAndType(contentUri, getMimeType(extension));
         }
         return intent;
